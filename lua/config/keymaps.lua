03204859e6a59d0b199ffaff0301e3f3299ac77e
x-wang-x <69587456+x-wang-x@users.noreply.github.com>
2026-03-11 23:11:03 +0700

    Initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- back from insert to normal
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
+
+-- files
+vim.api.nvim_set_keymap("n", "QQ", ":q!<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "WW", ":w!<enter>", { noremap = false })
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "embark-theme/vim", lazy = false, priority = 1000, name = "embark" },
+  { "EdenEast/nightfox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "navarasu/onedark.nvim", priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "carbonfox",
+    },
+  },
+}
