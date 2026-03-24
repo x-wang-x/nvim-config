1c8c5f5ea56d2d15d5ed975b1a0e4aa4ced1ac1a
x-wang-x <69587456+x-wang-x@users.noreply.github.com>
2026-03-25 01:58:04 +0700

    f : fold atomatically active when open file

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,7 +3,15 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
+
+--- clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- fold with ts
 vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()" 
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+--- make sure fold open when open file
+vim.opt.foldlevel = 99
+vim.opt.foldenable = false
