04e35208881e7b33d339841d316510480c525b36
x-wang-x <69587456+x-wang-x@users.noreply.github.com>
2026-03-27 17:51:22 +0700

    fix + upd : integrate confrom + mason

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,16 +1,29 @@
 return {
-  "stevearc/conform.nvim",
-  config = function()
-    require("conform").setup({
-      formatters_by_ft = {
-        python = { "black" },
-        rust = { "rustfmt" },
-      },
-    })
-
-    vim.keymap.set("n", "<leader>f", function()
-      require("conform").format()
-    end, { desc = "Format file" })
-  end,
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				markdown = { "prettier" },
+				json = { "prettier" },
+				html = { "prettier" },
+				css = { "prettier" },
+				["*"] = { "trim_whitespace" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+			format_after_save = false,
+			notify_on_error = true,
+			notify_no_formatters = false,
+		})
+		vim.keymap.set("n", "<leader>f", function()
+			require("conform").format()
+		end, { desc = "Format file" })
+	end,
 }
--- 
+--
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,56 +1,64 @@
 -- LSP keymaps — applied when any server attaches
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local map = function(keys, func, desc)
-      vim.keymap.set("n", keys, func, { buffer = args.buf, desc = desc })
-    end
+	callback = function(args)
+		local map = function(keys, func, desc)
+			vim.keymap.set("n", keys, func, { buffer = args.buf, desc = desc })
+		end
 
-    map("gd", vim.lsp.buf.definition, "Go to definition")
-    map("gr", vim.lsp.buf.references, "References")
-    map("K", vim.lsp.buf.hover, "Hover docs")
-    map("<leader>rn", vim.lsp.buf.rename, "Rename symbol")
-    map("<leader>ca", vim.lsp.buf.code_action, "Code action")
-    map("<leader>e", vim.diagnostic.open_float, "Line diagnostics")
-    map("[d", function() vim.diagnostic.jump({ count = -1 }) end, "Prev diagnostic")
-    map("]d", function() vim.diagnostic.jump({ count = 1 }) end, "Next diagnostic")
-  end,
+		map("gd", vim.lsp.buf.definition, "Go to definition")
+		map("gr", vim.lsp.buf.references, "References")
+		map("K", vim.lsp.buf.hover, "Hover docs")
+		map("<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+		map("<leader>ca", vim.lsp.buf.code_action, "Code action")
+		map("<leader>e", vim.diagnostic.open_float, "Line diagnostics")
+		map("[d", function()
+			vim.diagnostic.jump({ count = -1 })
+		end, "Prev diagnostic")
+		map("]d", function()
+			vim.diagnostic.jump({ count = 1 })
+		end, "Next diagnostic")
+	end,
 })
 
 -- Default config for ALL LSP servers (capabilities from cmp)
 vim.lsp.config("*", {
-  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
 
 return {
-  -- Mason (installer UI)
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
+	-- Mason (installer UI)
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-  -- Mason + LSP bridge — auto-installs & auto-enables servers
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "neovim/nvim-lspconfig",
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "rust_analyzer",
-          "pyright",
-        },
-        -- Automatically enable installed servers (default: true)
-        automatic_enable = true,
-      })
-    end,
-  },
+	-- Mason + LSP bridge — auto-installs & auto-enables servers
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"zapling/mason-conform.nvim",
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"rust_analyzer",
+					"pyright",
+				},
+				-- Automatically enable installed servers (default: true)
+				automatic_enable = true,
+			})
+			require("mason-conform").setup({
+				automatic_installation = true,
+			})
+		end,
+	},
 
-  -- LSP base (loaded by mason-lspconfig)
-  { "neovim/nvim-lspconfig" },
+	-- LSP base (loaded by mason-lspconfig)
+	{ "neovim/nvim-lspconfig" },
 }
