b2b19521ae91dc612c09300a82aceab205b786fa
x-wang-x <69587456+x-wang-x@users.noreply.github.com>
2026-03-29 00:50:41 +0700

    a : session name on status line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,20 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-require('lualine').setup({
-  options = {
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-  },
-})
-end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "nightfox",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_x = {
+					function()
+						return require("auto-session.lib").current_session_name()
+					end,
+				},
+			},
+		})
+	end,
 }
